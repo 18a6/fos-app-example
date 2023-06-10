37f922a46ca5944fc9766ac8f3dd959791467994
18a6 <ar.brhl.49@gmail.com>
2023-06-10 19:47:35 +0100

    _

---
+++ src/components/layouts/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="">
+      <b-navbar type="dark" variant="dark">
+        <b-navbar-nav>
+          <b-nav-item>
+            <router-link to="/" class="nav-link" href="#"
+              >Home</router-link
+            >
+          </b-nav-item>
+          <b-nav-item>
+            <router-link to="newAnime" class="nav-link" href="#"
+              >New Anime</router-link
+            >
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-navbar>
+    </div>
+   </template>
+   
+   
+   <script>
+   export default {};
+   </script>
+   
+   
+   <style>
+   </style>
+   
